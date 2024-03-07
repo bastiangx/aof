9c9daa7941ca5bbfbe05eb40e6570cf343ac968e
bastiangx <bastiangx@pm.me>
2024-03-07 01:07:45 +0000

    rebased from main

--- assets.py
+++ assets.py
@@ -1,30 +1,35 @@
-import SimpleGUICS2Pygame.simpleguics2pygame as sg
+from SimpleGUICS2Pygame.simpleguics2pygame import (
+    _load_local_image as get_image,
+)
+from os.path import join as pj
+from os import getcwd
 
-# from os.path import join as pj
-# from os import getcwd
-
+"""
 # Pathfinder
-IMAGE_PATH = 'assets/images'
-IMGAE_PATH = '/assets/images'
-ANIMATION_PATH = 'assets/animations'
-AUDIO_PATH = 'assets/audio'
-FONT_PATH = 'assets/fonts'
+Defines the paths for various assets
+"""
+
+IMAGE_PATH = getcwd() + '/assets/images/'
+ANIMATION_PATH = getcwd() + 'assets/animations/'
+AUDIO_PATH = getcwd() + 'assets/audio/'
+FONT_PATH = getcwd() + 'assets/fonts/'
 
 # Bullet stuff
-BULLET_IMG = sg.load_image('https://www.filterforge.com/filters/12594-v5.jpg')
-
+BULLET_IMG = get_image(pj(IMAGE_PATH, 'bullet.png'))
 
 # Player stuff
+PLAYER_IMG = get_image(pj(IMAGE_PATH, 'player.png'))
 
 # Zombie stuff
-ZOMBIE_IMG = sg.load_image(
-    'https://img.itch.zone/aW1nLzE0Mzk3ODY5LnBuZw==/original/6J9fF%2F.png'
-)
-
+ZOMBIE_IMG = get_image(pj(IMAGE_PATH, 'enemy-1.png'))
 
 # Background stuff
 
 # Barn / farm / base stuff
+# BARN_IMG = sg.load_image(
+#     'https://img.itch.zone/aW1nLzE0Mzk3ODY5LnBuZw==/original/6J9fF%2F.png'
+# )
+
 # Sound stuff
 
 # GUI stuff
