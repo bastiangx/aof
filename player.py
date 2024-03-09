ed86d0f872dc6513894ba3998b7c12406f3f0cfd
bastiangx <bastiangx@pm.me>
2024-03-09 21:31:16 +0000

    housekeeping
    deleted health fields

--- player.py
+++ player.py
@@ -1,15 +1,17 @@
 from config import CANVAS_WIDTH, CANVAS_HEIGHT
 from assets import PLAYER_IMG
 from vector import Vector
-from user_input import *
 
 
 class Player:
-    def __init__(self, health=100):
+    """
+    draws, updates and handles player movement
+    """
+
+    def __init__(self) -> None:
         self.velocity = Vector(1, 1)
         self.velocity.normalize()
         self.velocity_modifier = 8
-        # self.has_hit_wall = False
 
         self.image = PLAYER_IMG
 
@@ -17,11 +19,6 @@ class Player:
         self.y = CANVAS_HEIGHT / 1.1
         self.width = 80
         self.height = 80
-
-        self.health = health
-        self.max_health = 100
-
-        self.floor_damage = 2
 
     def draw(self, canvas):
         canvas.draw_image(
@@ -38,6 +35,7 @@ class Player:
 
     def move(self, keyboard):
         direction = Vector(0, 0)
+        # defines the vectorial direction
 
         if keyboard.up:
             direction.y -= 1
@@ -48,19 +46,8 @@ class Player:
         if keyboard.right:
             direction.x += 1
 
-        # self.has_hit_wall = False
-
         self.x += self.velocity.x * direction.x * self.velocity_modifier
         self.y += self.velocity.y * direction.y * self.velocity_modifier
-
-        # boundary check, if hit wall, bouce
-        # if self.x < 0 or self.x > CANVAS_WIDTH:
-        #     self.velocity.x *= -0.8
-        #     self.has_hit_wall = True
-        #
-        # if self.y < 0 or self.y > CANVAS_HEIGHT:
-        #     self.velocity.y *= -0.8
-        #     self.has_hit_wall = True
 
         self.x = max(min(self.x, CANVAS_WIDTH - 40), 40)
         self.y = max(min(self.y, CANVAS_HEIGHT - 40), 40)
@@ -70,35 +57,3 @@ class Player:
 
     def get_top_left(self):
         return (self.x - self.width / 2, self.y - self.height / 2)
-
-    def drw_test_hitbox(self, canvas):
-        sprite_top_left = (self.x - self.width / 2, self.y - self.height / 2)
-        canvas.draw_polygon(
-            [
-                (sprite_top_left[0], sprite_top_left[1]),
-                (sprite_top_left[0] + self.width, sprite_top_left[1]),
-                (
-                    sprite_top_left[0] + self.width,
-                    sprite_top_left[1] + self.height,
-                ),
-                (sprite_top_left[0], sprite_top_left[1] + self.height),
-            ],
-            2,
-            'blue',
-        )
-
-    def get_health(self):
-        return self.health
-
-    def get_max_health(self):
-        return self.max_health
-
-    def lose_floor_health(self):
-        if 0 < self.health <= self.max_health:
-            self.health -= self.floor_damage
-
-    def gain_health(self, heal):
-        self.health += heal
-
-    def is_dead(self):
-        return self.health <= 0
